9eb6cd8018a34feb981d8ec89061888b399436cf
Nicolas <ndonosofs@gmail.com>
2020-06-28 23:21:24 -0400

    App Setup

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+import Space from './Space.svelte';
+</script>
+
+<main>
+ <h1>
+ Tic Tac Toe
+ </h1>
+ <h2>Number of people playing: </h2>
+ <h2>Player X</h2>
+ <div class="row">
+<Space space="X" />
+<Space space="X" />
+<Space space="X" />
+</div>
+<div class="row">
+<Space space="X" />
+<Space space="X" />
+<Space space="X" />
+</div>
+<div class="row">
+<Space space="X" />
+<Space space="X" />
+<Space space="X" />
+</div>
+<button>New Game</button>
+</main>
+
+
+
+
+
+<style>
+main{
+    width:475px;
+    margin: 0 auto;
+    border: 0px black solid;
+    height:1000px;
+}
+.row{
+    display: flex;
+}
+button{
+    width:100%;
+    margin-top:20px;
+    border-radius:0;
+    background-color: lightblue;
+    font-size:30px;
+    cursor:pointer;
+    border:none;
+}
+button:hover{
+    outline: none;
+}
+</style>
---
+++ src/Space.svelte
@@ -0,0 +1,21 @@
+<script>
+export let space = "";
+</script>
+
+
+<div class="space">
+ {space}
+</div>
+
+<style>
+.space{
+    border: solid 1px gray;
+    height: 125px;
+    width: 125px;
+    flex: 1;
+    font-size: 75px;
+    line-height: 125px;
+    text-align: center;
+    cursor: pointer;
+}
+</style>
